eec742f23b6dc5c430dbcd51e785a3b56caaa529
Gustavo <gustavomouradossantos2@gmail.com>
2026-05-15 14:47:40 -0300

    .

--- src/_components/CardGrupo/cardgrupo.jsx
+++ src/_components/CardGrupo/cardgrupo.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./page.module.css";
+import styles from "./cardgrupo.module.css";
 
 export default function CardGrupo({ grupos = [] }) {
   function irParaGrupo(id) {
