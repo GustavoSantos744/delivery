b64ee20222c9c4d9c27ec029fcb8b72a1bd659ba
Gustavo <gustavomouradossantos2@gmail.com>
2026-05-15 13:59:34 -0300

    .

--- src/_components/Search/search.jsx
+++ src/_components/Search/search.jsx
@@ -1,22 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "./page.module.css";
 
-export default function CampoBusca() {
-  const [busca, setBusca] = useState("");
-
-  // Exemplo de produtos (depois você pode puxar da API)
-  const produtos = [
- 
-  ];
-
-  // Filtrar produtos
-  const produtosFiltrados = produtos.filter((produto) =>
-    produto.nome.toLowerCase().includes(busca.toLowerCase())
-  );
-
+export default function CampoBusca({ busca, setBusca }) {
   return (
     <div className={styles.container}>
-      {/* Campo de busca */}
       <div className={styles.boxBusca}>
         <input
           type="text"
@@ -25,20 +12,6 @@ export default function CampoBusca() {
           onChange={(e) => setBusca(e.target.value)}
           className={styles.inputBusca}
         />
-      </div>
-
-      {/* Lista */}
-      <div className={styles.lista}>
-        {produtosFiltrados.length > 0 ? (
-          produtosFiltrados.map((produto) => (
-            <div key={produto.id} className={styles.cardProduto}>
-              <span>{produto.nome}</span>
-              <small>{produto.grupo}</small>
-            </div>
-          ))
-        ) : (
-          <p className={styles.semResultado}>Teste..</p>
-        )}
       </div>
     </div>
   );
